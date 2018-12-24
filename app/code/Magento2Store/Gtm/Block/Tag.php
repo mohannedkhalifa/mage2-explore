d9ca2480a49c4ad3583a14f320ffe486a9e1bbfa
mohanned <khelifa.mohannad@gmail.com>
2018-12-24 15:53:57 +0100

    [MKH - gtm tag integration]

--- app/code/Magento2Store/Gtm/Block/Tag.php
+++ app/code/Magento2Store/Gtm/Block/Tag.php
@@ -11,7 +11,7 @@ use \Magento2Store\Gtm\Helper\Data;
 use \Magento\Tax\Helper\Data as TaxHelper;
 use \Magento\Checkout\Helper\Data as CheckoutHelper;
 use \Magento\Framework\Stdlib\DateTime\DateTimeFactory;
-use \Magento\Sales\Model\Order;
+use \Magento\Sales\Model\OrderFactory;
 use \Magento\Payment\Model\Config;
 
 class Tag extends \Magento\Framework\View\Element\Template
@@ -24,10 +24,23 @@ class Tag extends \Magento\Framework\View\Element\Template
     protected $_checkoutHelper;
     protected $_cmsPage;
     protected $_dateTimeFactory;
-    protected $_order;
+    protected $_orderFactory;
     protected $_paymentConfig;
 
-
+    /**
+     * Tag constructor.
+     * @param Context $context
+     * @param CustomerSession $customerSession
+     * @param CheckoutSession $checkoutSession
+     * @param Page $cmsPage
+     * @param Registry $registry
+     * @param Data $gtmHelper
+     * @param TaxHelper $taxHelper
+     * @param CheckoutHelper $checkoutHelper
+     * @param DateTimeFactory $dateTimeFactory
+     * @param OrderFactory $orderFactory
+     * @param Config $paymentConfig
+     */
     public function __construct(
         Context $context,
         CustomerSession $customerSession,
@@ -38,7 +51,7 @@ class Tag extends \Magento\Framework\View\Element\Template
         TaxHelper $taxHelper,
         CheckoutHelper $checkoutHelper,
         DateTimeFactory $dateTimeFactory,
-        Order $order,
+        OrderFactory $orderFactory,
         Config $paymentConfig
     )
     {
@@ -50,15 +63,16 @@ class Tag extends \Magento\Framework\View\Element\Template
         $this->_checkoutHelper  = $checkoutHelper;
         $this->_cmsPage         = $cmsPage;
         $this->_dateTimeFactory = $dateTimeFactory;
-        $this->_order           = $order;
+        $this->_orderFactory    = $orderFactory;
         $this->_paymentConfig   = $paymentConfig;
         parent::__construct($context);
     }
 
-
+    /**
+     * @return string
+     */
     public function getTag()
     {
-
         $data           = $this->_getDefaultTagData();
         $routeName      = $this->_request->getRouteName();
         $controllerName = $this->_request->getControllerName();
@@ -73,29 +87,20 @@ class Tag extends \Magento\Framework\View\Element\Template
         } elseif($currentProduct = $this->_registry->registry('current_product')) {
             $data['pageType'] = 'FicheProduit';
             $data             = array_merge($data, $this->getProductPageDataLayer($currentProduct));
-        } elseif($currentCategory = $this->_registry->registry('current_ca&tegory')) {
+        } elseif($currentCategory = $this->_registry->registry('current_category')) {
             $data['pageType'] = 'ListeProduit';
             $data             = array_merge($data, $this->getCategoryPageDataLayer());
-        } elseif($module == 'search' && $routeName == 'solrsearch' && $actionName == 'index') {
-            $data['pageType'] = 'Recherche';
-//                $data             = array_merge($data, $this->getSearchPageDataLayer());
-        } elseif($module == 'checkout' && $controllerName == 'cart' && $actionName == 'added') {
+        } elseif($module == 'checkout' && $controllerName == 'cart' && $actionName == 'add') {
             $data['pageType'] = 'RetourAjout';
+        } elseif($module == 'checkout' && $controllerName == 'index' && $actionName == 'index') {
+            $data['pageType'] = 'Panier';
+            $data             = array_merge($data, $this->getCartPageDataLayer());
         } elseif($module == 'checkout' && $controllerName == 'cart' && $actionName == 'index') {
             $data['pageType'] = 'Panier';
             $data             = array_merge($data, $this->getCartPageDataLayer());
         } elseif($module == 'customer' && $controllerName == 'account' && $actionName == 'login') {
             $data['pageType'] = 'login';
-        } elseif($module == 'checkout' && $controllerName == 'steps' && $actionName == 'info') {
-            $data['pageType'] = 'Informations client';
-            $data             = array_merge($data, $this->getCartPageDataLayer());
-        } elseif($module == 'checkout' && $controllerName == 'steps' && $actionName == 'shipping') {
-            $data['pageType'] = 'Livraison';
-            $data             = array_merge($data, $this->getCartPageDataLayer($data));
-        } elseif($module == 'checkout' && $controllerName == 'steps' && $actionName == 'payment') {
-            $data['pageType'] = 'ChoixPaiement';
-            $data             = array_merge($data, $this->getCartPageDataLayer($data));
-        } elseif($module == 'checkout' && $controllerName == 'steps' && $actionName == 'success') {
+        } elseif($module == 'checkout' && $controllerName == 'onepage' && $actionName == 'success') {
             $data['pageType'] = 'StatutPaiement';
             $data             = array_merge($data, $this->getConfirmationPageDataLayer($data));
         } elseif($module == 'customer' && $controllerName == 'account') {
@@ -105,7 +110,7 @@ class Tag extends \Magento\Framework\View\Element\Template
         }
 
         $dataScript = "<script type=\"text/javascript\">" . json_encode($data,
-                JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)
+                JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT)
             . "</script>";
         return $dataScript;
 
@@ -118,29 +123,25 @@ class Tag extends \Magento\Framework\View\Element\Template
     protected function getConfirmationPageDataLayer($data)
     {
         $orderId       = $this->_checkoutSession->getLastRealOrderId();
-        $order         = $this->_order->loadByIncrementId($orderId);
+        $order         = $this->_orderFactory->create()->loadByIncrementId($orderId);
         $types         = $this->_paymentConfig->getCcTypes();
-        $ccType        = $types[$order->getPayment()->getCcType()];
+        $ccType        = $order->getPayment()->getCcType() ? $types[$order->getPayment()->getCcType()] : null;
         $paymentMethod = ($ccType) ? $ccType : $order->getPayment()->getMethodInstance()->getTitle();
         $discountTotal = 0;
-        $nombreproduit = 0;
+        $nbProducts    = 0;
         $productIds    = '';
         $products      = [];
         $typeShipping  = [];
-        $allItems      = $order->getItemsCollection();
+        $allItems      = $order->getAllVisibleItems();
         foreach($allItems as $item) {
             $discountTotal += $item->getDiscountAmount();
-            $nombreproduit++;
+            $nbProducts++;
             $productIds = ($productIds != '') ? $productIds . '|' . $item->getProductId() : $item->getProductId();
-            $products[] = array('sku'      => $item->getSku(), 'name' => $item->getName(),
-                                'category' => $item->getMainCategory(), 'price' => number_format($item->getPrice(), 2, '.', ''), 'quantity' => intval($item->getQtyOrdered()));
-//            $shipping = "";
-//            if($item->getMiraklShippingTypeLabel())
-//                $shipping = $item->getMiraklShippingTypeLabel();
-//            else if($order->getShippingCarrier()->getCarrierCode())
-//                $shipping = $this->_arrayShippingCorrespondance[$order->getShippingCarrier()->getCarrierCode()];
-//            if(!in_array($shipping,$typeShipping))
-//                $typeShipping[] = $shipping;
+            $products[] = ['sku'      => $item->getSku(),
+                           'name'     => $item->getName(),
+                           'category' => $item->getMainCategory(),
+                           'price'    => number_format($item->getPrice(), 2, '.', ''),
+                           'quantity' => intval($item->getQtyOrdered())];
         }
         if($order->getCouponCode()) {
             $data['transactionPromoCode'] = $order->getCouponCode();
@@ -148,30 +149,27 @@ class Tag extends \Magento\Framework\View\Element\Template
             $data['code_promo_montant']   = $discountTotal;
         }
         $data['transactionPaymentType'] = $paymentMethod;
-        // $isNewCustomer = $this->_customerSession->getCustomer()->getIsNewCustomer();
-        // $data['typeclient'] = (isset($isNewCustomer) && $isNewCustomer == '0') ? 'ancien client' : 'nouveau client';
-
+        $isNewCustomer                  = $this->_customerSession->getCustomer()->getIsNewCustomer();
+        $data['typeclient']             = (isset($isNewCustomer) && $isNewCustomer == '0') ? 'ancien client' : 'nouveau client';
         arsort($typeShipping);
         $data['transactionShippingMethod'] = implode("_", $typeShipping);
-        //$data['emailclient'] = md5($order->getCustomerEmail());
-        //$data['nombreproduit'] = $nombreproduit;
-        //$data['panieridproduit'] = $productIds;
-        $data['basketType']      = "Standard";
-        $data['transactionType'] = Mage::helper('gsgtm')->getTransactionType($order);
-        $data['transactionId']   = $orderId;
-        //$data['transactionAffiliation'] = "";
-        $data['transactionTotal']    = $order->getGrandTotal() - $order->getTaxAmount();
-        $data['transactionTotalTTC'] = $order->getGrandTotal();
-        $data['transactionTax']      = $order->getTaxAmount();
-        $data['transactionShipping'] = $order->getShippingAmount() + 1 / 1.2 * $order->getMiraklShippingFee();
-        $data['transactionCurrency'] = ucfirst(strtolower($order->getOrderCurrencyCode()));
-        $data['transactionProducts'] = $products;
+        $data['emailclient']               = md5($order->getCustomerEmail());
+        $data['nombreproduit']             = $nbProducts;
+        $data['basketType']                = "Standard";
+        $data['transactionType']           = __('OPERATEUR');
+        $data['transactionId']             = $orderId;
+        $data['transactionTotal']          = $order->getGrandTotal() - $order->getTaxAmount();
+        $data['transactionTotalTTC']       = $order->getGrandTotal();
+        $data['totalDiscount']             = $discountTotal;
+        $data['transactionTax']            = $order->getTaxAmount();
+        $data['transactionShipping']       = $order->getShippingAmount();
+        $data['transactionCurrency']       = ucfirst(strtolower($order->getOrderCurrencyCode()));
+        $data['transactionProducts']       = $products;
+        $oCreatedDateTime                  = $order->getCreatedAt();
+        $creationDate                      = $oCreatedDateTime;
+        $data['transactionDate']           = $creationDate;
+        $data['event']                     = "purchaseEvent";
 
-        $oCreatedDateTime        = $order->getCreatedAt();
-        $creationDate            = $oCreatedDateTime;
-        $data['transactionDate'] = $creationDate;
-
-        $data['event'] = "purchaseEvent";
         return $data;
     }
 
@@ -180,9 +178,8 @@ class Tag extends \Magento\Framework\View\Element\Template
      */
     protected function getCartPageDataLayer()
     {
-
         $products = [];
-        foreach($this->_checkoutSession->getQuote()->getAllItems() as $item) {
+        foreach($this->_checkoutSession->getQuote()->getAllVisibleItems() as $item) {
             $products[] = array('sku' => $item->getSku(), 'name' => $item->getName()
             , 'price'                 => number_format($item->getPrice(), 2, '.', ''), 'quantity' => $item->getQty());
         }
@@ -247,41 +244,16 @@ class Tag extends \Magento\Framework\View\Element\Template
         if($currentProduct->getTypeId() == 'simple') {
             $data['productSku'] = $currentProduct->getSku();
         }
-        $data['dispoSite']      = "disponible";
-        $currentPrice           = $currentProduct->getFinalPrice();
-        $data['productPrice']   = number_format($currentPrice, 2, '.', '');
-        $data['productSticker'] = "";
-//        $productCategoryTree=Mage::helper('gsgtm')->getProductCategoryTree($currentProduct);
-//        for($i=0;$i<count($productCategoryTree);$i++){
-//            if($i==5){
-//                break;
-//            }
-//            $data['productCategory'.($i+1)]=$productCategoryTree[$i]['name'];
-//        }
-//        $data['productInStock'] = ($currentProduct->getIsInStock())?"1":"0";
-        $data['productSource'] = "Standard";
-//        $data['isMarketPlace'] = false;
-//        if($currentProduct->getIsMarketplaceOrigin()){
-//            $offer = Mage::helper('mirakl_connector/offer')->getOfferById($currentProduct->getBestOfferId());
-//            if($offer && $offer->getShopId() != 0){
-//                $data['productSource'] = "MKP";
-//                $data['isMarketPlace'] = true;
-//            }
-//        }
-//        $brand = $this->helper('gscatalog')->getAttributeText('marque', $currentProduct);
-//        $data['productTrademark'] = !empty($brand)?$brand:"";
-
+        $data['dispoSite']            = "disponible";
+        $currentPrice                 = $currentProduct->getFinalPrice();
+        $data['productPrice']         = number_format($currentPrice, 2, '.', '');
+        $data['productSticker']       = "";
+        $data['productSource']        = "Standard";
         $priceWithoutDiscount         = $currentProduct->getPrice();
         $priceWithoutDiscount         = number_format($priceWithoutDiscount, 2, '.', '');
-        $unitPriceTaxFree             = $this->_taxHelper->getPrice($currentProduct, $priceWithoutDiscount, false);
-        $unitPriceTaxFree             = number_format($unitPriceTaxFree, 2, '.', '');
         $data['unitPriceTaxIncluded'] = $priceWithoutDiscount;
-        $data['unitPriceTaxFree']     = $unitPriceTaxFree;
         $currentPrice                 = number_format($currentPrice, 2, '.', '');
-        $discountTaxFree              = $this->_taxHelper->getPrice($currentProduct, $currentPrice, false);
-        $discountTaxFree              = number_format($discountTaxFree, 2, '.', '');
-        $data['discountTaxIncluded']  = $currentPrice;
-        $data['discountTaxFree']      = $discountTaxFree;
+        $data['priceTaxIncluded']     = $currentPrice;
 
         return $data;
     }
@@ -301,7 +273,9 @@ class Tag extends \Magento\Framework\View\Element\Template
         return $data;
     }
 
-
+    /**
+     * @return string
+     */
     public function toHtml()
     {
         if(!$this->_gtmHelper->getConfig('tag/general/enable')) {
